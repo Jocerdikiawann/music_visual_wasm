63f02262bcdf7431de9e97594db3fddd0b89b60f
Nur Kholid <cocokcocok33@gmail.com>
2024-02-17 02:48:03 +0700

    fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,34 @@
 
 set -xe
 
-emcc -o game.html main.c -Os -Wall $HOME/Documents/raylib/src/libraylib.a -I. -I$HOME/Documents/raylib/src/ -L. -L$HOME/Documents/raylib/src/ -s USE_GLFW=3 -s ASYNCIFY --shell-file $HOME/Documents/raylib/src/shell.html -DPLATFORM_WEB
+Platform="web"
+
+while getopts ":p:" opt; do
+  case ${opt} in
+    p )
+      Platform=$OPTARG
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ "$Platform" = "web" ] ; then
+    echo "Building for web..."
+    emcc -o game.html main.c -Os -Wall $HOME/Documents/raylib/src/libraylib.a -I. -I$HOME/Documents/raylib/src/ -L. -L$HOME/Documents/raylib/src/ -s USE_GLFW=3 -s ASYNCIFY --shell-file $HOME/Documents/raylib/src/shell.html -DPLATFORM_WEB
+elif [ "$Platform" = "android" ] ; then
+    echo "Building for android..."
+elif [ "$Platform" = "desktop" ] ; then
+  echo "Building for desktop..."
+else
+    echo "Invalid platform: $Platform"
+    exit 1
+fi
+
