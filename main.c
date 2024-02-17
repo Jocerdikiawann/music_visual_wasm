ec5beb38f82e2776781bf00a178d36f9894844d6
Nur Kholid <cocokcocok33@gmail.com>
2024-02-17 21:35:16 +0700

    fix

--- main.c
+++ main.c
@@ -1,23 +1,65 @@
 #include <raylib.h>
+#include <stdbool.h>
 
 int main() {
-  InitWindow(800, 450, "Music Visualizer");
+  int screenWidth = 800;
+  int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "Music Visualizer");
 
   InitAudioDevice();
 
-  Sound sound = LoadSound("./videoplayback.ogg");
+  Music music = LoadMusicStream("./videoplayback.ogg");
+
+  music.looping = false;
+
   SetTargetFPS(60);
 
+  float volume = 0.5f;
+  float timeplayed = 0.0f;
+  bool pause = false;
+
   while (!WindowShouldClose()) {
-    if (IsKeyPressed(KEY_SPACE))
-      PlaySound(sound);
+    UpdateMusicStream(music);
+
+    if (IsKeyPressed(KEY_SPACE)) {
+      StopMusicStream(music);
+      PlayMusicStream(music);
+    }
+
+    if (IsKeyPressed(KEY_P)) {
+      pause = !pause;
+      if (pause)
+        PauseMusicStream(music);
+      else
+        ResumeMusicStream(music);
+    }
+
+    if (IsKeyDown(KEY_DOWN)) {
+      volume -= 0.1f;
+    } else if (IsKeyDown(KEY_UP)) {
+      volume += 0.1f;
+    }
+
+    SetMusicVolume(music, volume);
+
+    timeplayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music) *
+                 (screenWidth - 40);
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Press SPACE to play sound", 200, 100, 20, LIGHTGRAY);
+    DrawRectangle(20, screenHeight - 20 - 12, screenWidth - 40, 12, LIGHTGRAY);
+    DrawRectangle(20, screenHeight - 20 - 12, (int)timeplayed, 12, MAROON);
+    DrawRectangleLines(20, screenHeight - 20 - 12, screenWidth - 40, 12, GRAY);
+    DrawText("Press SPACE to PLAY/STOP sound", 40, 40, 20, BLACK);
+    DrawText("Press P to pause sound", 40, 70, 20, BLACK);
+    DrawText("Press ARROW UP / ARROW DOWN to change volume", 40, 100, 20,
+             BLACK);
+    DrawText(TextFormat("VOLUME: %f", volume), 40, 130, 20, MAROON);
     EndDrawing();
   }
 
-  UnloadSound(sound);
+  UnloadMusicStream(music);
 
   CloseAudioDevice();
   CloseWindow();
