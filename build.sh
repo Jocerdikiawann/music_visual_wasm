1d0f1903a41b5114c3a5bdfd1fe06861dc244012
Nur Kholid <cocokcocok33@gmail.com>
2024-02-17 17:43:23 +0700

    fix gitignore

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ shift $((OPTIND -1))
 
 if [ "$Platform" = "web" ] ; then
     echo "Building for web..."
-    emcc -o game.html main.c -Os -Wall -Wextra $HOME/Documents/raylib/src/libraylib.a -I. -I$HOME/Documents/raylib/src/ -L. -L$HOME/Documents/raylib/src/ -s USE_GLFW=3 -s ASYNCIFY --shell-file $HOME/Documents/raylib/src/shell.html -DPLATFORM_WEB --preload-file ./videoplayback.ogg -s ALLOW_MEMORY_GROWTH=1
-    emrun game.html
+    emcc -o main.html main.c -Os -Wall -Wextra $HOME/Documents/raylib/src/libraylib.a -I. -I$HOME/Documents/raylib/src/ -L. -L$HOME/Documents/raylib/src/ -s USE_GLFW=3 -s ASYNCIFY --shell-file $HOME/Documents/raylib/src/shell.html -DPLATFORM_WEB --preload-file ./videoplayback.ogg -s ALLOW_MEMORY_GROWTH=1
+    emrun main.html
 elif [ "$Platform" = "android" ] ; then
     echo "Building for android..."
 elif [ "$Platform" = "desktop" ] ; then
